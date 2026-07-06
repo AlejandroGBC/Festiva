7eb77bcf33217eacd67b2c5068d42256f931ee17
ndrtte <andreamorelaleman@gmail.com>
2026-07-06 13:44:42 -0600

    pathname y navbar con hooks nativos de next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import Navbar from "@/shared/components/Navbar";
+
 export default function Home() {
-  
+    return (
+        <Navbar/>
+    );
 }
--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,4 +1,8 @@
-import { Home, PartyPopper, MessageSquare, User } from "lucide-react";
+"use client";
+
+import { Home, PartyPopper, MessageSquare, User} from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from 'next/link';
 
 const navLinks = [
   {
@@ -24,17 +28,19 @@ const navLinks = [
 ];
 
 export function Navbar() {
+  const pathname = usePathname();
   return (
-    <nav className="flex w-full items-center justify-between rounded-xl border border-[#C9C4D0] bg-[#F1ECF1] px-2 py-2">
+    <nav className="flex w-full items-center justify-between rounded-xl border border-[#C9C4D0] bg-[#F1ECF1] p-2">
       {navLinks.map((link) => {
         const Icon = link.icon;
+        const isActive = pathname === link.href;
         return (
-          <a key={link.href} href={link.href} className="group flex flex-1 flex-col items-center rounded-full py-2 transition-colors hover:bg-festiva-euphoric-pink">
-            <Icon size={18} className="text-[#48454F] transition-colors group-hover:text-[#590027]" />
-            <span className="mt-1 text-[11px] text-[#48454F] transition-colors group-hover:text-[#590027] font-medium tracking-[0.5px]">
+          <Link key={link.href} href={link.href} className={`group flex flex-1 flex-col items-center rounded-full py-1 transition-colors ${isActive ? "bg-festiva-euphoric-pink" : "hover:bg-festiva-euphoric-pink"} `}>
+            <Icon size={18} className={`text-[#48454F] transition-colors ${isActive ? "text-[#590027]" : "group-hover:text-[#590027]" }`}/>
+            <span className={`mt-1 text-[11px] text-[#48454F] transition-colors ${isActive ? "text-[#590027]" : "group-hover:text-[#590027]" } font-medium tracking-[0.5px]`}>
               {link.label}
             </span>
-          </a>
+          </Link>
         );
       })}
     </nav>
