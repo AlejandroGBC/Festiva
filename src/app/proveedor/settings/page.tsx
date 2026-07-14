1ae318e3df982b113f8903945ca653b469c6d1f7
Karla Santos <karlapatriciasr21@gmail.com>
2026-07-14 16:23:42 -0600

    fix: uso de ruta estandar

--- src/app/proveedor/settings/page.tsx
+++ src/app/proveedor/settings/page.tsx
@@ -1,11 +1,13 @@
-'use client';
-
-import SettingPage from '@/modules/proveedor/configuracion/components/SettingsPage';
+import SettingPage from '@/modules/proveedor/pages/SettingsPage';
 import Navbar from '@/shared/components/Navbar';
+export const metadata = {
+    title: 'Festiva - Configuración',
+    description: '',
+};
 
 export default function Page() {
   return (
-    <main>
+    <main className="flex-1 flex flex-col min-h-0 w-full overflow-hidden">
       <SettingPage/>
       <Navbar />
     </main>
